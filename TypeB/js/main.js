766affdf626c04ef516e624d3821cc02eb647e04
cd-mk <cd-mk@cidow.com>
2019-08-08 13:30:03 +0900

    b타입 메인 작업완료 (gnb영역 제외)

--- TypeB/js/main.js
+++ TypeB/js/main.js
@@ -1,1 +1,25 @@
 // TypeB Main page JS
+$(document).ready(function () {
+    new WOW().init();
+
+
+    // $('.bestPro-slide .swiper-slide').on('mouseenter', function () {
+    //     $(this).find('.text').addClass('fadeInUp');
+    //     $(this).find('.text').addClass('animated');
+    //     $(this).find('.img img').addClass('animated');
+    //     $(this).find('.img img').addClass('zoomIn');
+    // });
+    // $('.bestPro-slide .swiper-slide').on('mouseleave', function () {
+    //     $(this).find('.text').removeClass('fadeInUp');
+    //     $(this).find('.text').removeClass('animated');
+    //     $(this).find('.img img').removeClass('animated');
+    //     $(this).find('.img img').removeClass('zoomIn');
+    // });
+
+
+
+
+
+  
+    
+});
