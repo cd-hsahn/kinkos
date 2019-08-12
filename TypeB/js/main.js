279908622dfa4eb12a31fc745bd3714001f20bcf
hongsun <hongsun3152@gmail.com>
2019-08-12 13:25:57 +0900

    TypeB main gnb 수정

--- TypeB/js/main.js
+++ TypeB/js/main.js
@@ -22,37 +22,75 @@ $(document).ready(function () {
     })();
 
     // sub gnb show / hide
-    (function () {
-        $('.gnb > li > a').on('mouseenter', function () {
-            var title = $(this).text();
-            $('.gnb > li > a').addClass('not_on');
-            $(this).removeClass('not_on').addClass('on');
+    // (function () {
+    //     $('.gnb > li > a').on('mouseenter', function () {
+    //         var title = $(this).text();
+    //         $('.gnb > li > a').addClass('not_on');
+    //         $(this).removeClass('not_on').addClass('on');
+    //         $('.sub_gnb').stop().slideDown(500);
+
+    //         $('.js-snb-title').text(title);
+
+    //         if ($(this).hasClass('js-location')) {
+    //             $('.sub_gnb_item').hide();
+    //             $('.location_item').show();
+    //         } else {
+    //             $('.sub_gnb_item').show();
+    //             $('.location_item').hide();
+    //         }
+    //         $('.dim').show();
+    //     });
+    //     $('.gnb > li > a').on('mouseleave', function () {
+    //         $('.gnb > li > a').removeClass('on');
+    //     });
+    //     $('#header').on('mouseleave', function () {
+    //         $('.gnb > li > a').removeClass('not_on');
+    //         $('.sub_gnb').stop().slideUp(500);
+    //         $('.dim').hide();
+    //     });
+    //     $('.header_top').on('mouseenter', function () {
+    //         $('.gnb > li > a').removeClass('not_on');
+    //         $('.sub_gnb').stop().slideUp(500);
+    //         $('.dim').hide();
+    //     });
+    // })();
+    
+    (function() {
+        $('.gnb > li > a').on('mouseenter', function() {
+            var idx = $(this).parent().index();
+
+            $('.gnb > li > a').removeClass('on');
+            $(this).addClass('on');
             $('.sub_gnb').stop().slideDown(500);
 
-            $('.js-snb-title').text(title);
+            if (idx !== 4) {
+                var left = 500;
+                var width = 226;
+                var move = idx * width;
 
-            if ($(this).hasClass('js-location')) {
-                $('.sub_gnb_item').hide();
-                $('.location_item').show();
-            } else {
                 $('.sub_gnb_item').show();
                 $('.location_item').hide();
+
+                $('.point').css('left', left + move + 'px');
+            } else {
+                $('.sub_gnb_item').hide();
+                $('.location_item').show();
             }
-            $('.dim').show();
         });
-        $('.gnb > li > a').on('mouseleave', function () {
+
+        $('.header_top').on('mouseenter', function() {
             $('.gnb > li > a').removeClass('on');
-        });
-        $('#header').on('mouseleave', function () {
-            $('.gnb > li > a').removeClass('not_on');
             $('.sub_gnb').stop().slideUp(500);
-            $('.dim').hide();
         });
-        $('.header_top').on('mouseenter', function () {
-            $('.gnb > li > a').removeClass('not_on');
+        $('#header').on('mouseleave', function() {
+            $('.gnb > li > a').removeClass('on');
             $('.sub_gnb').stop().slideUp(500);
-            $('.dim').hide();
+        });
+
+        $('.gnb_links > li > a').on('mouseenter', function() {
+            $('.gnb_links > li > a').addClass('not_on');
+            $(this).removeClass('not_on');
+            $(this).addClass('on');
         });
     })();
-    
 });
